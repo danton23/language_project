0c0deee517f80ae7b51cf16b4dd662e496cb9885
Charlie <melvins_fan@hotmail.com>
2019-06-13 22:04:01 +0100

    Added Class that creates the Language Windows instead of having to do it individually - need to tweak it so it adds correct vocab to each one in the TestGenerator class

--- User_gui2.py
+++ User_gui2.py
@@ -4,6 +4,7 @@ from tkinter import messagebox
 from Germ_cats import *
 from functools import partial
 from TestGen import TestGenerator
+from Span_cats import *
 import random
 
 
@@ -13,37 +14,41 @@ Masterlabel=Label(root, text="Please choose a Language to work on today!")
 Masterlabel.pack(side=TOP)
 var=IntVar()
 var.set(1)
-def Germwindow():
-    Germroot=tkinter.Tk()
-    Germroot.geometry("500x500")
-    var=IntVar()
-    var.set(0)
-    button_identities = []
-    words=[]
-    words+=categories
-    print("this is words " + str(words))
-    GermTest=TestGenerator()
-    print(GermTest)
-    GermTest.hoola
-    
-    
-              
-    for word in words:
-                    
-                     Radiobutton=tkinter.Radiobutton(Germroot, text=word, command=lambda x=word: GermTest.Change( x) )   #you need a lambda to pass the particular WORD of the item being iterated over into the function as argument (otherwise it only does this AFTER all buttons created and passes LAST word in)
+class Langwindow:
+    wordslist=categories
+    def make(self):
+        Langroot=tkinter.Tk()
+        Langroot.geometry("500x500")
+        words=[]
+        words+=Langwindow.wordslist
+        print("this is words " + str(words))
+        LangTest=TestGenerator()
+        for word in words:
+            Radiobutton=tkinter.Radiobutton(Langroot, text=word, command=lambda x=word: LangTest.Change(x))#you need a lambda to pass the particular WORD of the item being iterated over into the function as argument (otherwise it only does this AFTER all buttons created and passes LAST word in)
         #here HAD command=partial(Change)) and worked
-                     
-                     Radiobutton.pack(side=tkinter.LEFT) 
-def Russwindow():
-    Rusroot = tkinter.Tk()
-    Rusroot.geometry("500x500")
-def Spanwindow():
-    Spanroot = tkinter.Tk()
-    Spanroot.geometry("500x500")
-w= Radiobutton(root, text ="German", variable=var,value=1, command=Germwindow)
+
+            Radiobutton.pack(side=tkinter.LEFT)
+    def test(self):
+        print("working!")
+#newwindow=Langwindow.create(self)
+Spanwindow=Langwindow()
+print(Spanwindow)
+Spanwindow.test()
+Spanwindow.wordslist=SpanCategories
+print(Spanwindow.wordslist)
+Russwindow=Langwindow()
+Russwindow.test()
+Germwindow=Langwindow()
+
+
+
+#def Spanwindow():
+#    Spanroot = tkinter.Tk()
+ #   Spanroot.geometry("500x500")
+w= Radiobutton(root, text ="German", variable=var,value=1, command=Germwindow.make)
 w.pack(anchor=W)
-w1 =Radiobutton(root, text ="Russian",variable=var, value=2, command=Russwindow)
+w1 =Radiobutton(root, text ="Russian",variable=var, value=2, command=Russwindow.make)
 w1.pack(anchor=W)
-w3=Radiobutton(root, text ="Spanish", variable=var, value=3, command=Spanwindow)
+w3=Radiobutton(root, text ="Spanish", variable=var, value=3, command=Spanwindow.make)
 w3.pack(anchor=W)
 root.mainloop()
