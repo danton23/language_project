b793d17a6432ce746907ad2e65480900bf6defe8
Charlie <melvins_fan@hotmail.com>
2019-06-13 17:06:46 +0100

    Gui now uses a TestGenerator Class instead of a function so can apply to other languages

--- User_gui2.py
+++ User_gui2.py
@@ -3,6 +3,7 @@ from tkinter import *
 from tkinter import messagebox
 from Germ_cats import *
 from functools import partial
+from TestGen import TestGenerator
 import random
 
 
@@ -20,85 +21,16 @@ def Germwindow():
     button_identities = []
     words=[]
     words+=categories
-    print(words)
-    def Change(x):
-              Wordroot = tkinter.Tk()
-              Wordroot.geometry("500x500")
-              Windowlabel=Label(Wordroot, text=x)
-              Windowlabel.pack()
-              name=StringVar()
-              def genlist():
-                 for key, value in categories.items():
-                   if key == x:
-                     print ("this is "+ key)
-                     print ("this is categories [x]")
-                     print(categories[x])
-                     testcats=categories[x]   #here we are splitting the categories dict into only the part that conforms to the original category the user selected (i.e abstract, verbs etc)
-                     return testcats
-                
-              testcats=genlist()
-              def genkey():
-                  key=random.choice(list(testcats))
-                  Germlabel=Label(Wordroot, text="How do you say " + key + " in English?")
-                  Germlabel.pack()
-                  return key
-              key=genkey()
-              
-             
-              e1 = Entry(Wordroot, textvariable=name)
-              def checkinput(name):
-                  
-                  name=e1.get()  
-                  for k, v in testcats.items():
-                      if k ==key:
-                         targetval=v
-                         
+    print("this is words " + str(words))
+    GermTest=TestGenerator()
+    print(GermTest)
+    GermTest.hoola
     
-                  with open ("times.txt", "r") as f:
-                          contents=f.read()
-                          print (contents)
-                  numbcontents=int(contents)
-                  print("this is target value " + targetval)
-                  print(name)
-                  if numbcontents<=5:
-                      print("less")
-                      numbcontents+=1
-                      print("this is newnumb " + str(numbcontents))
-                      
-                      if name==targetval:
-                                              print("well done!")
-                                              Wordroot.destroy()
-                                              Change(x)
-                                                 
-                      elif name!=targetval:
-                                              with open ("times.txt","w")as f:
-                                                    f.write(str(numbcontents))
-                                              with open ("times.txt", "r") as f:
-                                                    contents=f.read()
-                                                    print("this is new contents "+ contents)
-                                              print ("try again!")
-                                              
-                  else:
-                      
-                      if v[0:2]=="to":
-                           
-                           
-                           print("to " + v[3:6] + " here are the first three letters of the word!")
-                           with open("times.txt", "w") as f:
-                                f.write("1")
-                          
-                      else:
-                                print(v[:3] + " here are the first three letters of the word! ")
-                                with open("times.txt", "w") as f:
-                                  f.write("1")
-                          
-                  
-              e1.bind("<Return>",checkinput)
-              e1.pack()
+    
               
     for word in words:
                     
-                     Radiobutton=tkinter.Radiobutton(Germroot, text=word, command=lambda x=word: Change(x))   #you need a lambda to pass the particular WORD of the item being iterated over into the function as argument (otherwise it only does this AFTER all buttons created and passes LAST word in)
+                     Radiobutton=tkinter.Radiobutton(Germroot, text=word, command=lambda x=word: GermTest.Change( x) )   #you need a lambda to pass the particular WORD of the item being iterated over into the function as argument (otherwise it only does this AFTER all buttons created and passes LAST word in)
         #here HAD command=partial(Change)) and worked
                      
                      Radiobutton.pack(side=tkinter.LEFT) 
