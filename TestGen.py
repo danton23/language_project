c39b2aebbcbede20e63ffa68dcdd2b9cc4a4a860
Charlie <melvins_fan@hotmail.com>
2019-06-19 14:51:43 +0100

    have updated TestGenerator and functions so that it is able to repeatedly generate new vocab test - problem was with class/inheritance and pulling/generating appropriate data after function had been called first time

--- TestGen.py
+++ TestGen.py
@@ -1,30 +1,65 @@
 import tkinter
 import random
 from Germ_cats import *
+# from Spanish_cats import *
 from tkinter import *
 class TestGenerator:
     file="times.txt"
+    def __init__(self, vocab):
+       
+        self.vocab=vocab
+
+     
+
+#    def __init__(self,parent):
+        
+  #      Wordroot = Frame(parent)
+ #       Wordroot.pack()
+ #       print("this is parent " + str(parent))
+    
+    
     
     def hoola(self ):
         print("hoola!")
-    def Change(self, x):
-        
-              Wordroot = tkinter.Tk()
-              Wordroot.geometry("500x500")
+        print("this is self.vocab " +str(self.vocab))
+        print("this is "+ str(self.vocab))
+    
+    def Change(event,self,x):
+
+              print("this is self.wordslist"  + str(self.wordslist))
+              
+              print("this is event " + str(event))
+              print(event)
+              print("this is self " +str(self))
+              print(self)
+              print("this is X " + str(x))
+              print(x)
+              Wordroot = Tk()
+              Wordroot.geometry=("500x500")
               Windowlabel=Label(Wordroot, text=x)
               Windowlabel.pack()
               name=StringVar()
-              def genlist():
-                 for key, value in categories.items():
+        #      def action(self):
+       #           print("destroyed")
+      #            Langwindow.destroy()
+     #         Wordroot.bind("<Destroy>", action)
+              
+             
+              def genlist(formattedwords):
+                 for key, value in formattedwords:
                    if key == x:
                      print ("this is "+ key)
                      print ("this is categories [x]")
                      print(categories[x])
-                     testcats=categories[x]   #here we are splitting the categories dict into only the part that conforms to the original category the user selected (i.e abstract, verbs etc)
+                     testcats=self.wordslist[x]   #here we are splitting the categories dict into only the part that conforms to the original category the user selected (i.e abstract, verbs etc)
                      return testcats
-                
-              testcats=genlist()
+              
+              
+              formattedwords=self.wordslist.items()
+              print("this is formattedwords " + str(formattedwords))
+              testcats=genlist(formattedwords)
               def genkey():
+                  print("this is " + str(testcats))
                   key=random.choice(list(testcats))
                   Germlabel=Label(Wordroot, text="How do you say " + key + " in English?")
                   Germlabel.pack()
@@ -33,14 +68,14 @@ class TestGenerator:
               
              
               e1 = Entry(Wordroot, textvariable=name)
-              def checkinput(self):
-                  file=TestGenerator.file
+              def checkinput(event):
+                 
                   name=e1.get()  
                   for k, v in testcats.items():
                       if k ==key:
                          targetval=v
                          
-    
+                  file=TestGenerator.file
                   with open (file, "r") as f:
                           contents=f.read()
                           print (contents)
@@ -55,7 +90,33 @@ class TestGenerator:
                       if name==targetval:
                                               print("well done!")
                                               Wordroot.destroy()
-                                              TestGenerator.Change(self,x)
+                                              print("this is original x" )
+                                              keylist=[]
+                                              for k in self.wordslist:
+                                                  keylist.append(k)
+                                              for k, v in testcats.items():
+                                                  currentkey=k
+                                              print(currentkey + "this is currentkey")
+
+                                              print("current dict= " + str(self.wordslist))
+    
+
+                                              currentdic=self.wordslist
+                                              def iteratedict(currentdic):
+                                                  for x in currentdic:
+                                                       if currentkey in currentdic[x]:
+                                                                  print (str(currentkey) +  " is currentkey")
+                                                                  print((str(currentdic[x])) + "this is currentdic[x]")
+                                                                  print(str(x) + "this is x")
+                                                                  newkey=x
+                                                                  return newkey
+                                              newkey=iteratedict(currentdic)
+                                              print("this is newkey " + str(newkey))
+                                              print(" here are keys %s"%(keylist))
+                                              
+                                              
+                                              LangTest=TestGenerator(newkey)
+                                              TestGenerator.Change(event,self,x)
                                                  
                       elif name!=targetval:
                                               with open (file,"w")as f:
--- User_gui2.py
+++ User_gui2.py
@@ -12,43 +12,80 @@ root = tkinter.Tk()
 root.geometry("500x500")
 Masterlabel=Label(root, text="Please choose a Language to work on today!")
 Masterlabel.pack(side=TOP)
-var=IntVar()
-var.set(1)
+Rootvar=IntVar()
+Rootvar.set(1)
+print("this is SpanCategories " + str(SpanCategories))
 class Langwindow:
-    wordslist=categories
+    def __init__(self, wordslist):
+        
+         self.wordslist=wordlist
+    butvar=IntVar()
+    butvar.set(99)
+    def __init__(self):
+       
+       self.ident=""
+    
     def make(self):
-        Langroot=tkinter.Tk()
+        Langroot=Toplevel(root)
+        
         Langroot.geometry("500x500")
         words=[]
-        words+=Langwindow.wordslist
+        words+=self.wordslist
         print("this is words " + str(words))
-        LangTest=TestGenerator()
-        for word in words:
-            Radiobutton=tkinter.Radiobutton(Langroot, text=word, command=lambda x=word: LangTest.Change(x))#you need a lambda to pass the particular WORD of the item being iterated over into the function as argument (otherwise it only does this AFTER all buttons created and passes LAST word in)
-        #here HAD command=partial(Change)) and worked
+        LangTest=TestGenerator(self.wordslist)
+        print("this is LangTest.vocab " + str(LangTest.vocab))
+        LangTest.hoola()
+ #      LangTest.parent=Langroot
+        self.radVar=IntVar()
+        self.radVar.set(99)
+        
+        buttonlist=[]
+        list=[]
+        iterlist=0
+        
+       
 
+        
+                         
+
+        var=IntVar()
+        var.set(99)
+        values=[0,1,2]
+
+        for word in words:
+            
+            #list+=1 #use this to create values for the radiobutton value (determines if is on or off) this works!!
+     #       print("this is current list value "+str(list)) + "this is current var "+str(var.get())
+            iterlist+=1
+            print(list)
+            Radiobutton=tkinter.Radiobutton(Langroot, text=word,value=iterlist, variable=var,command=lambda self=self,  x=word: LangTest.Change(self,x)) #vocab=words
+            buttonlist.append(Radiobutton)
+            #you need a lambda to pass the particular WORD of the item being iterated over into the function as argument (otherwise it only does this AFTER all buttons created and passes LAST word in)
+        #here HAD command=partial(Change)) and worked 
+            if list == 0:Radiobutton.select()
             Radiobutton.pack(side=tkinter.LEFT)
     def test(self):
         print("working!")
 #newwindow=Langwindow.create(self)
 Spanwindow=Langwindow()
-print(Spanwindow)
-Spanwindow.test()
 Spanwindow.wordslist=SpanCategories
 print(Spanwindow.wordslist)
 Russwindow=Langwindow()
+Russwindow.wordslist=categories
+Russwindow.ident=Russwindow
 Russwindow.test()
 Germwindow=Langwindow()
+Germwindow.wordslist=categories
 
 
 
 #def Spanwindow():
 #    Spanroot = tkinter.Tk()
  #   Spanroot.geometry("500x500")
-w= Radiobutton(root, text ="German", variable=var,value=1, command=Germwindow.make)
+w= Radiobutton(root, text ="German", variable=Rootvar,value=1, command=Germwindow.make)
 w.pack(anchor=W)
-w1 =Radiobutton(root, text ="Russian",variable=var, value=2, command=Russwindow.make)
+w1 =Radiobutton(root, text ="Russian",variable=Rootvar, value=2, command=Russwindow.make)
 w1.pack(anchor=W)
-w3=Radiobutton(root, text ="Spanish", variable=var, value=3, command=Spanwindow.make)
+w3=Radiobutton(root, text ="Spanish", variable=Rootvar, value=3, command=Spanwindow.make)
 w3.pack(anchor=W)
 root.mainloop()
