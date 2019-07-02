90d730d5576a836f6daca32cafd94a053b8fe28a
Charlie <melvins_fan@hotmail.com>
2019-07-02 19:16:10 +0100

    Updated USer_gui working on  being able to dynamically generate multiple pages to display all stored vocab in given category, i.e VERBS, so that User can delete items

--- User_gui2.py
+++ User_gui2.py
@@ -73,6 +73,8 @@ class Langwindow:
             UpdateFrame.pack(fill="both", expand="True")
             testvar=IntVar()
             testvar.set(1)
+
+        
             
             
                     
@@ -129,28 +131,33 @@ class Langwindow:
                             WordRoot2.destroy()
                             WordRoot3=Tk()
                             WordRoot3.geometry=("500x500")
-                            Windowlabel=Label(WordRoot3, text="please enter the new English word")
+                            Windowlabel=Label(WordRoot3, text="please enter the new English word")   #NOTE HERE IT CANNOT DEAL WITH SPACES SO IT THINKS VERBS ARE JUST "TO
                             Windowlabel.pack()
                             v=StringVar()
                             e2=Entry(WordRoot3, textvariable=v)
                             def checkvalue3(event,self):
                                 tempname2=e2.get()
+                                print(str(tempname2)+ " is TEMPNAME2 ")
+                               #list(map(str,input().split()))
                                 with open("tempwords.txt", "a") as f:
-                                   f.write(tempname2)
+                                   tempname3=str(tempname2)
+                                   
+                                   f.write(tempname3)
                                 newlist=[]
                                 with open("tempwords.txt", "r") as f:
                                     
                                     lines=f.readlines()
                                     for i in lines:
                                         thisline=i.split(" ")
-                                    print(thisline)
+                                    print("this is this line" + str(thisline))
                                     key=thisline[0]
-                                    value=thisline[1]
+                                    value=tempname2
+                                    print(str(value)+ "VALUE") 
                                     print("key is " + key + " value is " + value)
                                     print (str(self.wordslist) + "THis is Langwindow.wordslist ")
                                     print("key is " + key)
                                     print("this is current " + x)     ### WORK ON THIS this checks currently held values - vocab etc  - THIS ONE tells current category being manipulated
-                                    print("this is words " + str(words))  ###this shows ALL values within language group 
+                                    print("this is words " + str(self.wordslist))  ###this shows ALL values within language group 
                               #      words+={key:value}
                                     for item in self.wordslist:
                                         if item == x:
@@ -216,14 +223,149 @@ class Langwindow:
                   #  Radiobutton(row=2, column=iterlist)
             templist=0
             
-            for word in words:
+            for word in self.wordslist:
                 print("this is templist!")
                 templist+=1
                 Radiobutton=tkinter.Radiobutton(UpdateFrame, text=word, variable=testvar, value=templist,command=lambda x=word:AddWord(x), padx=50)
                 Radiobutton.pack(side=LEFT)
-            print (words)
+            #print (words)
         Radiobutton=tkinter.Radiobutton(Langroot, text="click to update a category", variable=testvar ,bg="red", value=0, command=UpdateWords)
         Radiobutton.pack(side=BOTTOM)
+        def RemoveWords():
+            Removeroot=Toplevel(root)
+            Removeroot.geometry("300x300")
+            Removelabel=Label(Removeroot, text = "Please choose a category to edit")
+            Removelabel.pack(side="top")
+            Removeframe=Frame(Removeroot, height="150", width="150", bg="green")
+            Removeframe.pack(fill="both", expand="True")
+            testvar=IntVar()
+            testvar.set(1)
+            templist=0
+            def RemoveWord(x):
+                RWroot=Toplevel(root)
+                RWroot.geometry("500x500")
+                RWLabel=Label(RWroot, text = "Choose an item to remove!")
+                RWLabel.pack(side="top")
+                RWFrame=Frame(RWroot, height="500", width="500") #containter Frame
+                RWFrame.pack(side=TOP,fill="both", expand="True")
+                RWFrame2=Frame(RWFrame, height="250", width="250", bg="yellow")
+                RWFrame2.pack(side=TOP, fill="both",expand="True")
+                RWFrame1=Frame(RWFrame, height="250", width="250", bg="blue")
+                RWFrame1.pack(side=TOP, fill="both", expand="True")
+                templist2=0
+                buttonlist2=[]
+                
+                print(x + "THIS IS CURRENT X!")
+                def DestroyWord(y):
+                    print(y)
+                for item in self.wordslist:
+                    if item ==x:
+                        c=0
+                        for y in self.wordslist[item]:
+                            c+=1
+                print("number of items" + str(c))
+                numberofrows=c/3
+                print(str(numberofrows) + ": number of rows")
+                numberofscreens=numberofrows/5 #5 rows in a screen
+                print(str(numberofscreens) + ": number of screens")
+                screencount=0
+                rowcount=0
+                irow=0
+                col=0
+                templist2=0
+                buttonlist2=[]
+                
+                currentcol=["red","blue","green"]
+               
+                
+                for item in self.wordslist:
+                    if item ==x:   #first check are in correct cat i.e VERBS 
+                       useword=item
+                currentcol=["red","blue","green"]  #want to cycle thru this
+                col=0
+                colit=0
+                it=0       
+                for y in self.wordslist[useword]: #for each entry in item (i.e verbs)
+                           print(y)
+                           if col==2:
+                                    print(str(y)+"col==3")
+                                    col=0
+                           
+                           if colit==0:
+                                    print(str(y)+"colit==0")
+                                    print(str(currentcol[col])+"current col is")
+                                    RWFramei=tkinter.Frame(RWFrame, height="100", width="100", bg=currentcol[col])
+                                    RWFramei.pack(side=TOP, fill="both",expand="True")
+                           elif colit==2:
+                                print(str(y)+"colit ==2")
+                                
+                                col+=1
+                                RWFramei=Frame(RWFrame, height="100", width="250", bg=currentcol[col])
+                                print(str(currentcol[col])+ "current col is")
+                                RWFramei.pack(fill="both",expand="True")
+                                
+                          
+                           Radiobutton3=tkinter.Radiobutton(RWFramei, text=y, variable=testvar, value=templist2, command =lambda y=y:DestroyWord(y))
+                           Radiobutton.pack(side=LEFT, padx=50)
+                           buttonlist2.append(Radiobutton3)
+                           templist2+=1
+                           it+=1
+                           colit+=1
+                               
+                               
+                                   
+                               
+                               #####REINSERT CODE BELOW HERE WITH ELIF IN LINE WITH COLIT+=1
+                                
+                           
+                           
+                        # elif col ==2:
+                         #    Radiobutton3=tkinter.Radiobutton(RWFrame2, text=y, variable=testvar, value=templist2, command = lambda y=y:DestroyWord(y))
+                          #   buttonlist2.append(Radiobutton3)
+                           #  templist2+=1
+                            # col=0
+                print("buttonlist2" + str(buttonlist2))
+                i=0
+                for item in buttonlist2:
+                    try:
+                      i+=1
+                      valuecast=(buttonlist2[i].config("value"))   #this is finding value of each radiobutonn - as only a certian number fit on page need to find this and tehn split into different frames/windows
+                      print(str(valuecast)+"VALCAST")
+                      valuecast3=int(valuecast[4])
+                      print(str(valuecast3) + "THIS IS VALCAST3!")
+                      if valuecast3<=4:
+                          print("ooga")
+                          print(str(buttonlist2[i].master))
+                         # buttonlist2[i].update(parent=RWFrame2)
+                          buttonlist2[i].pack(side=LEFT,padx=50)
+                      elif valuecast3 >=4<6:
+                          print("gooober")
+                          
+                          buttonlist2[i].pack(side=LEFT, padx=50)
+                   #   elif valuecast3 > 3 :
+                    #      buttonlist2[i].config(RWFrame2)
+                     #     print(str(buttonlist2[i].master))
+                      #    buttonlist2[i].pack(side=LEFT, padx = 50)
+                    except:
+                         pass
+                    # except:
+                     
+                      #print(buttonlist2)
+                     # pass
+            
+                    
+                
+               
+               # for item in buttonlist2:
+                #                 if item.value <3:
+                 #                  item.pack(side=LEFT, padx=50)
+            for word in self.wordslist:
+                print("this is templist!")
+                templist+=1
+                Radiobutton=tkinter.Radiobutton(Removeframe, text=word, variable=testvar, value=templist,command=lambda x=word:RemoveWord(x), padx=50)
+                Radiobutton.pack(side=LEFT)
+        Radiobutton2=tkinter.Radiobutton(Langroot, text="click to edit a category", variable=testvar, bg="red", value=1, command=RemoveWords)
+        Radiobutton2.pack(side=BOTTOM)
         WindLabel=Label(Langroot, text="Please choose a category to test!", bg="red")
         WindLabel.pack(side="top")
         wordframe=Frame(Langroot, height="250", width="250", bg="blue")
@@ -257,14 +399,46 @@ def WordsGen(file):
                     words.update({k:v})
     print("this is RUswords NOW!" + str(words))
     return (words)
+def current_languages():
+    languages=[]
+    with open("languages.json", "r")as f:
+        data=json.load(f)
+        count1=0
+        for k, v in data.items():
+            for item in v:
+                count1+=1
+        print("count 1 is")
+                
+        count=0 
+        while count< count1:
+          for k,v in data.items():
+              for item in v:
+                count+=1
+                languages.append(item)
+                print("languages is " +str(languages))
+        else:
+               return languages
+        
+languages=current_languages()
+print("these are languages!" + str(languages))
+
+for item in languages:
+    print (item)
+    Newwind=Langwindow(str(item)+"words", str(item)+"_cats.json", str(item)+".json")
+    print (Newwind.usefile)    #way of dynamically checking existing languages - try to figure out how to use this to generate Radiobuttons!
+    
 Rusfile="Rus_cats.json"
 Ruswords=WordsGen(Rusfile)
 Russwindow=Langwindow(Ruswords,Rusfile, "Russvocabcheck.json")
 
 #Russwindow.ident=Russwindow
 Russwindow.test()
-Germwindow=Langwindow(categories, Rusfile, "Germvocabcheck.json")  #use last variable to check how many times a word guessed correctly in Langtest
-Spanwindow=Langwindow(SpanCategories, Rusfile, "Spanvocabcheck.json")
+Germfile="Germ_cats.json"
+Germwords=WordsGen(Germfile)
+Germwindow=Langwindow(Germwords, Germfile, "Germvocabcheck.json")  #use last variable to check how many times a word guessed correctly in Langtest
+Spanfile="Span_cats.json"
+Spanwords=WordsGen(Spanfile)
+Spanwindow=Langwindow(Spanwords, Spanfile, "Spanvocabcheck.json")
 #Germwindow.wordslist=categories
 
 
